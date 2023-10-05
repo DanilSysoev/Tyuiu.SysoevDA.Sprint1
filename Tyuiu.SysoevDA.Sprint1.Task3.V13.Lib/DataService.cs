c83055bf0d901c628658a20c4e3feb5b7e195a01
SysoevDA <fuzzii2739@gmail.com>
2023-10-05 19:03:58 +0500

    Task3

---
+++ Tyuiu.SysoevDA.Sprint1.Task3.V13.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.SysoevDA.Sprint1.Task3.V13.Lib
+{
+    public class DataService : ISprint1Task3V13
+    {
+        public double MultiplyOfDigits(double number)
+        {
+            int num1, num2, num3;
+
+            num3 = (int)(number % 10.0);
+            num2 = (int)((number / 10.0) % 10.0);
+            num1 = (int)(number / 100.0);
+            var res = num1 * num2 * num3;
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.SysoevDA.Sprint1.Task3.V13.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SysoevDA.Sprint1.Task3.V13.Lib;
+
+namespace Tyuiu.SysoevDA.Sprint1.Task3.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 123;
+            var res = ds.MultiplyOfDigits(x);
+            Assert.AreEqual(6,res);
+        }
+    }
+}
