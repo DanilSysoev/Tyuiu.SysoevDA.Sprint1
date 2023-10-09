03aac662a2ec221ff5a84e630280892ae6bc00a1
SysoevDA <fuzzii2739@gmail.com>
2023-10-09 13:02:54 +0500

    Task6

---
+++ Tyuiu.SysoevDA.Sprint1.Task6.V9.Lib/DataService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.SysoevDA.Sprint1.Task6.V9.Lib
+{
+    public class DataService : ISprint1Task6V9
+    {
+        public string MoveLetterToStart(string value)
+        {
+            string[] words = value.Split(' ');
+            string res = "";
+            foreach (string word in words)
+            {
+                if (word.Length < 2)
+                {
+                    res = $"{res}{word} ";
+                }
+                else
+                {
+                    char lastLetter = word[word.Length - 1];
+                    string restOfWord = word.Substring(0, word.Length - 1);
+                    string modifiedWord = lastLetter + restOfWord;
+                    res = $"{res}{modifiedWord} ";
+                }
+            }
+            return res.Remove(res.Length - 1);
+        }
+    }
+}
---
+++ Tyuiu.SysoevDA.Sprint1.Task6.V9.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SysoevDA.Sprint1.Task6.V9.Lib;
+
+namespace Tyuiu.SysoevDA.Sprint1.Task6.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            string value = "Название проектов";
+
+            var res = ds.MoveLetterToStart(value);
+            Assert.AreEqual("еНазвани впроекто", res);
+        }
+    }
+}
